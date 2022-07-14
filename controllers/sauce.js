e5434cc9eda8dd62da3c6e5e42d77605e6f9a557
Camille <84985836+CamilleBarr@users.noreply.github.com>
2022-07-15 00:25:58 +0200

    adaptation of images in post, sauce controllers

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -1,18 +1,24 @@
-const sauce = require('../models/sauce');
-
+const Sauce = require('../models/sauce');
+const fs = require('fs');
 //---------- réponse retourné par le serveur en CREATION / POST
 exports.createSauce = (req, res, next) => {
-    delete req.body._id;
-    const sauce = new sauce({
-        ...req.body
+    const sauceObject = JSON.parse(req.body.sauce);
+    delete sauceObject._id;
+    delete sauceObject._userId;
+    const sauce = new Sauce({
+        ...sauceObject,
+        userId: req.auth.userId,
+        imageUrl:'${req.protocol}://${req.get("host")}/images/${req.file.filename}'
     });
+
     sauce.save()
-        .then(res.status(201).json({
-            message: 'objet enregistré'
-        }))
+        .then(() => {res.status(201).json({
+            message: 'objet enregistré !'
+})})
         .catch(error => res.status(400).json({
-            error
+            error 
         }));
+
 };
 
 exports.updateSauce = ((req, res, next) => {
