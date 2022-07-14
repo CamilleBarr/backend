571db2603eee1ede41e6aa0798cedb9551fcb096
Camille <84985836+CamilleBarr@users.noreply.github.com>
2022-07-14 22:44:56 +0200

    modification of routes with multer

--- routes/sauce.js
+++ routes/sauce.js
@@ -1,13 +1,22 @@
 const express = require('express');
 //const app = require('../app');
-
-const auth = require('../middleware/auth');
 const router = express.Router();
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
+
 
 const sauceControllers = require('../controllers/sauce');
 
+
+router.get('/', auth, sauceControllers.getAllSauce); 
+
 // //---------- réponse retournée par le serveur en MODIFICATION / MAJ /PUT via le fichier controllers
-router.post('/', auth, sauceControllers.createSauce);
+router.post('/', auth, multer, sauceControllers.createSauce);
+
+
+//---------- réponse retourné par le serveur en RECUPERATION / GET
+router.get('/:id', auth, sauceControllers.getOneSauce);
 
 //---------- réponse retourné par le serveur en MODIFICATION / MAJ /PUT
 router.put('/:id',auth, sauceControllers.updateSauce);
@@ -15,8 +24,5 @@ router.put('/:id',auth, sauceControllers.updateSauce);
 //---------- réponse retourné par le serveur en SUPPRESSION / DESTROY
 router.delete('/:id',auth, sauceControllers.deleteSauce);
 
-//---------- réponse retourné par le serveur en RECUPERATION / GET
-router.get('/:id', auth, sauceControllers.getOneSauce);
-router.get('/', auth, sauceControllers.getAllSauce); 
 
 module.exports = router;
