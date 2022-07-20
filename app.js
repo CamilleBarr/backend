b669f95d3c5230ccacbd9eee135579700866d351
Camille <84985836+CamilleBarr@users.noreply.github.com>
2022-07-20 09:23:38 +0200

    adding security parameters to app.js

--- app.js
+++ app.js
@@ -3,13 +3,19 @@ const express = require('express');
 //--------- j'appelle la fonction mongoose
 const mongoose = require('mongoose');
 //---- inportation des modèles
-//--------- j'importe le modèle des sauces
-const sauceRoutes = require('./routes/sauce');
 //--------- j'importe le modèle d'authentification utilisateur
 const userRoutes = require('./routes/user');
+//--------- j'importe le modèle des sauces
+const sauceRoutes = require('./routes/sauce');
 
+const path = require('path');
 //--------- j'appelle la fonction express
 const app = express();
+//---------- ajout de sécurité pour les hearders et les variables
+/* const dotenv = require('dotenv').config(); 
+const helmet = require('helmet'); 
+app.use(helmet());
+*/
 //---------- connexion à la base de données MongoDB. Ci-dessous = middleware
 mongoose.connect('mongodb+srv://WebmisstressTest0:WebmisstressTest0@cluster0.ngbkf.mongodb.net/?retryWrites=true&w=majority', {
         useNewUrlParser: true,
@@ -17,29 +23,30 @@ mongoose.connect('mongodb+srv://WebmisstressTest0:WebmisstressTest0@cluster0.ngb
     })
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
-
+/* OPTIONAL FOR PIQUAANTE
 app.post(express.json());
 app.post((req, res) => {
     res.json({
         message: 'Votre requête a bien été reçue !'
     });
 });
+*/
 
 app.use(express.json());
 
 //---------- headers adapté pour les CORS 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    //res.setHeader('Cross-Origin-Resource-Policy', 'same-site'); ajout securité
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
 
-const path = require('path');
 
 //---------- on enregistre les routes comme ceci :
-app.use('/api/sauce', sauceRoutes);
 app.use('/api/auth', userRoutes);
+app.use('/api/sauce', sauceRoutes);
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
