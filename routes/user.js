9b94c634134a7d873eb20c15ff3c3f43fb4b38d6
Camille <84985836+CamilleBarr@users.noreply.github.com>
2022-07-20 09:40:56 +0200

    adding security parameters to route user

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+//const maxConnectionRequet = require('../middleware/limiter');
 const userControllers = require('../controllers/user');
 
 //route post parce que frontend envoie aussi des info : address mail and passwords
@@ -8,6 +9,8 @@ const userControllers = require('../controllers/user');
 router.post('/signup', userControllers.signup);
 
 //---------- réponse retourné par le serveur en MODIFICATION / MAJ /PUT
-router.post('/login', userControllers.login);
+router.post('/login', 
+//max.limiter,
+userControllers.login);
 
 module.exports = router;
