3ee1627cbc9e209bc0e7b82a211860c5fb65be40
Camille <84985836+CamilleBarr@users.noreply.github.com>
2022-08-01 10:47:03 +0200

    adjusting rooter path for login

--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,17 @@
 const express = require('express');
-
+const router = express.Router();
 //const maxConnectionRequet = require('../middleware/limiter');
 const userController = require('../controllers/user');
 const email = require ('../middleware/email');
 const password = require('../middleware/password');
 
-const router = express.Router();
+
 //route post parce que frontend envoie aussi des info : address mail and passwords
 
 // //---------- réponse retournée par le serveur en MODIFICATION / MAJ /PUT via le fichier controllers
 router.post('/signup', email, password, userController.signup);
 
 //---------- réponse retourné par le serveur en MODIFICATION / MAJ /PUT
-router.post('/login', email, password, userController.login);
+router.post('/login', userController.login);
 
 module.exports = router;
