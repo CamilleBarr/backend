f4a68f85f22fe9b83214c1db9d53e3d44b97bb42
Camille <84985836+CamilleBarr@users.noreply.github.com>
2022-08-02 10:51:22 +0200

    clearing code on user controller

--- controllers/user.js
+++ controllers/user.js
@@ -2,57 +2,46 @@ const bcrypt = require('bcrypt');
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv').config();
-//let emailValidator = require('email-validator');
-//const regex = ('^[a-zA-Z.-_]{3,30}[@]{1}[a-zA-Z.-_]{3,30}[.]{1}[a-z]{2}[^0-9]$');
-//const passwordValidator = require('password-validator');
 
+//Ce plugin permet de sécuriser l'adresse email
 //const cryptJs = require('crypto-js'); 
-
-
 
 //---------- inscription avec mot de passe sécurisé et class user / async
 exports.signup = (req, res, next) => {
-    
-console.log("email signing up : ", req.body.email);
-console.log("pwd in signing up: ", req.body.password);
-   /*
-    if (emailValidator.validate(req.body.email) = (req.body.email)) {
-        throw "email address invalid"
-        
-    } else 
-    if (schemaPassword.validate((req.body.password) != (req.body.password))) {
-        return "password invalid. Must contain between 8 to 25 characters, capital and lower letters, with 2 numbers at least "
-    } else {*/
-        
-        bcrypt.hash(req.body.password, 10)
-            .then(hash => {
-                const user = new User({
-                    email: req.body.email,
-                    password: hash
-                });
-                user.save()
-                    .then(() => res.status(201).json({
-                        message: "Utilisateur crée et sauvegardé !"
-                    }))
-                    .catch(error => res.status(400).json({
-                        error
-                    }));
-            })
-            .catch(error => res.status(500).json({
+
+    console.log("email signing up : ", req.body.email);
+    console.log("pwd in signing up: ", req.body.password);
+
+    bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+            const user = new User({
+                email: req.body.email,
+                password: hash
+            });
+            user.save()
+                .then(() => res.status(201).json({
+                    message: "Utilisateur crée et sauvegardé !"
+                }))
+                .catch(error => res.status(400).json({
+                    error
+                }));
+        })
+        .catch(error => res.status(500).json({
                 error
             }
             //.send(console.log(error))
-            ));
-            
-        //}
+        ));
+
+    //}
 };
 
 //---------- connexion à la plateforme avec vérification compte existant et verification password
 // avec gestion d'erreur d'exécution de la requete au serveur, err verif mot de passe, err user not exist
 exports.login = (req, res, next) => {
-    
-console.log("email loging up : ", req.body.email);
-console.log("pwd in loging up: ", req.body.password);
+
+    console.log("email loging up : ", req.body.email);
+    console.log("pwd in loging up: ", req.body.password);
+
     User.findOne({
             email: req.body.email
         })
@@ -74,16 +63,16 @@ console.log("pwd in loging up: ", req.body.password);
                                 token: jwt.sign({
                                         userId: user._id
                                     },
-                                    process.env.TOKEN_SECRET,{ 
+                                    process.env.TOKEN_SECRET, {
                                         expiresIn: '24h'
                                     }
                                 )
                             });
                         }
                     })
-                    
+
                     .catch(error => {
-                        console.log ("hash crypt compare has fail with :", error.message);
+                        console.log("hash crypt compare has fail with :", error.message);
                         res.status(500).json({
                             error
                         });
@@ -91,10 +80,10 @@ console.log("pwd in loging up: ", req.body.password);
             }
         })
         .catch(error => {
-            console.log ("mongoDB fetch  has fail with :", error.message);
+            console.log("mongoDB fetch  has fail with :", error.message);
             res.status(500).json({
                 error
             })
         });
-        
+
 };
