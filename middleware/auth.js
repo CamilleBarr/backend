212b1a313c4381a28af4fa9ebd12c04c8c86875b
Camille <84985836+CamilleBarr@users.noreply.github.com>
2022-08-02 16:04:08 +0200

    bugfix on displaying main page auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,16 +1,28 @@
 const jwt = require('jsonwebtoken');
+
+const dotenv = require("dotenv").config();
 module.exports = (req, res, next) => {
+    
+    
+    console.log("req.body", req.body);
     try {
+        if (!req.headers.authorization) {
+            console.log("req.headers:,", req.headers.authorization);
+            throw "Merci de vous connecter";
+          }
         const token = req.headers.authorization.split(" ")[1];
-        const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
+        const decodedToken = jwt.verify(token, `${process.env.TOKEN_SECRET}`);
         const userId = decodedToken.userId;
+        console.log("req.body.user, userId 1", req.body.email, userId);
 
         if (req.body.userId && req.body.userId !== userId) {
+            console.log("req.body.user, userId invalid", req.body.userId, userId);
             throw "Invalid user"
         } else {
-            next()
+            next();
         }
     } catch (error) {
+        console.log("catch");
         res.status(403).json({
             message :"Unauthorized request"
         });
